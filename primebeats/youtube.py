09930b65527697dda1bef3aed7703675da65adef
vasugmr9-hash <vasugmr9@gmail.com>
2026-09-06 09:54:37 +0530

    Update and rename primebeats_youtube_FINAL_HYBRID.py to youtube.py

--- primebeats/youtube.py
+++ primebeats/youtube.py
@@ -38,7 +38,7 @@ from .state import Track
 API_URL = os.environ.get(
     "MEOW_API_URL", "https://music.yukiapi.site"
 ).rstrip("/")
-API_KEY = os.environ.get("MEOW_API_KEY", "").strip()
+API_KEY = os.environ.get("MEOW_API_KEY", "yuki_f13c54a24cae79023a43f41e794a3dfc").strip()
 
 DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "downloads")
 
